c22583068667b871ef6c4370919d757ef1f4943e
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-11-22 07:47:44 +0700

    Update ss3-4b.c

--- ss3-4b.c
+++ ss3-4b.c
@@ -1,14 +1,15 @@
 #include<stdio.h> 
 int main(){
-	float diemtoan, diemvan, diemanh,tbc;
+	float diemtoan, diemvan, diemanh,tbc,tong;
 	printf("nhap diem van\n: ");
 	scanf("%f",&diemvan) ;
 	printf("nhap diem toan\n: ");
 	scanf("%f",&diemtoan) ;
 	printf("nhap diem anh: \n");
 	scanf("%f",&diemanh) ;
+	tong=diemvan+diemtoan+diemanh;
 	tbc=(diemvan+diemtoan+diemanh)/3; 
-	printf("diem trung binh cong ba mon l‡ %f",tbc) ;
+	printf("diem trung binh cong ba mon l√† %f",tbc) ;
 	return 0; 
 
 } 
